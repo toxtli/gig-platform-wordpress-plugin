4c997696d4ded7c799c3564b7acb24254285bdc8
Carlos Toxtli <ctoxtli@gmail.com>
2017-10-19 18:04:16 -0400

    Caldera forms magic tags for option elements

---
+++ content/index.js
@@ -0,0 +1,24 @@
+jQuery(function(){
+jQuery(".fillable").each(function(){
+  var inputs = jQuery(this).find("input");
+  if (inputs.length > 0) {
+    inputs.each(function(){
+      var option = this;
+      var fieldValue = jQuery(option).val();
+      var matches = fieldValue.match(/\{local\:(.*?)\}/);
+      if (matches.length > 1) {
+        var field = matches.pop();
+        var fieldObject = jQuery("." + field + " input");
+        console.log(field);
+        console.log(fieldObject.val());
+        jQuery(option).val(fieldObject.val());
+        jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldObject.val());
+        fieldObject.on("input", function() {
+          jQuery(option).val(fieldObject.val());
+          jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldObject.val());
+        });
+      }
+    });  
+  }
+});
+});
