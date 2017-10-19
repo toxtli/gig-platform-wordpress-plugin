d4da493dcd8e3eb19b24e56dd16824325dde736d
Carlos Toxtli <ctoxtli@gmail.com>
2017-10-19 18:17:38 -0400

    Hide empty elements

--- content/index.js
+++ content/index.js
@@ -4,18 +4,31 @@ jQuery(function(){
     if (inputs.length > 0) {
       inputs.each(function(){
         var option = this;
+        var parentElement = jQuery(this).parent().parent();
         var fieldValue = jQuery(option).val();
         var matches = fieldValue.match(/\{local\:(.*?)\}/);
         if (matches.length > 1) {
           var field = matches.pop();
           var fieldObject = jQuery("." + field + " input");
+          var fieldValue = fieldObject.val();
           console.log(field);
-          console.log(fieldObject.val());
-          jQuery(option).val(fieldObject.val());
-          jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldObject.val());
+          console.log(fieldValue);
+          jQuery(option).val(fieldValue);
+          jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldValue);
+          if (fieldValue == "") {
+            parentElement.hide();
+          } else {
+            parentElement.show();
+          }
           fieldObject.on("input", function() {
-            jQuery(option).val(fieldObject.val());
-            jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldObject.val());
+            var fieldValue = fieldObject.val();
+            jQuery(option).val(fieldValue);
+            jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldValue);
+            if (fieldValue == "") {
+              parentElement.hide();
+            } else {
+              parentElement.show();
+            }
           });
         }
       });  
