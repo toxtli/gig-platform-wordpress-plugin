08e17a43efd26f2ea2daf952bbeea59cc3d50f84
Carlos Toxtli <ctoxtli@gmail.com>
2017-10-23 16:03:10 -0400

    Update index.js

--- content/index.js
+++ content/index.js
@@ -1,18 +1,42 @@
 jQuery(function(){
+  $('body').on('DOMNodeInserted', '.fillable', function(e) {
+    if (e.target.tagName != "LABEL") {
+      gigConnectFields(this);
+    }
+  });
   jQuery(".fillable").each(function(){
-    var inputs = jQuery(this).find("input");
-    if (inputs.length > 0) {
-      inputs.each(function(){
-        var option = this;
-        var parentElement = jQuery(this).parent().parent();
-        var fieldValue = jQuery(option).val();
-        var matches = fieldValue.match(/\{local\:(.*?)\}/);
-        if (matches.length > 1) {
-          var field = matches.pop();
-          var fieldObject = jQuery("." + field + " input");
-          var fieldValue = fieldObject.val();
+    gigConnectFields(this);
+  });
+});
+
+function gigConnectFields(source) {
+  var inputs = jQuery(source).find("input");
+  if (inputs.length > 0) {
+    inputs.each(function(){
+      var option = this;
+      var parentElement = jQuery(this).parent().parent();
+      var fieldValue = jQuery(option).val();
+      var matches = fieldValue.match(/{local:(.*?)}/);
+      if (matches.length > 1) {
+        var field = matches.pop();
+        var fieldQuery = "." + field + " input";
+        var fieldObject = jQuery(fieldQuery);
+        var fieldValue = "";
+        if (fieldObject.length > 0) {
+          fieldValue = fieldObject.val();
           console.log(field);
           console.log(fieldValue);
+          jQuery(option).val(fieldValue);
+          jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldValue);
+        }
+        if (fieldValue == "") {
+          parentElement.hide();
+        } else {
+          parentElement.show();
+        }
+        $(document).on("input", fieldQuery, function() {
+          var fieldObject = jQuery(fieldQuery);
+          var fieldValue = fieldObject.val();
           jQuery(option).val(fieldValue);
           jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldValue);
           if (fieldValue == "") {
@@ -20,18 +44,8 @@ jQuery(function(){
           } else {
             parentElement.show();
           }
-          fieldObject.on("input", function() {
-            var fieldValue = fieldObject.val();
-            jQuery(option).val(fieldValue);
-            jQuery("label[for='" + option.id + "']").contents().last().replaceWith(" " + fieldValue);
-            if (fieldValue == "") {
-              parentElement.hide();
-            } else {
-              parentElement.show();
-            }
-          });
-        }
-      });  
-    }
-  });
-});
+        });
+      }
+    });  
+  }
+}
